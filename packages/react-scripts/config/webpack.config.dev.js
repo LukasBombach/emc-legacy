97347e990d675d372d24b3926895fd2d676b7edc
Lukas Bombach <lukas.bombach@googlemail.com>
2017-11-26 21:56:03 +0100

    added custom @elmc/babel-preset-react-app

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -169,7 +169,7 @@ module.exports = {
             options: {
               // @remove-on-eject-begin
               babelrc: false,
-              presets: [require.resolve('babel-preset-react-app')],
+              presets: [require.resolve('@elmc/babel-preset-react-app')],
               // @remove-on-eject-end
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
