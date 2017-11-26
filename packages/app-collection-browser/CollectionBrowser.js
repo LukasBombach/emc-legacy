39b34dda4cd18e2a838ea74dc5af8b86b3bb6235
Lukas Bombach <lukas.bombach@googlemail.com>
2017-11-26 20:41:05 +0100

    dabbling around with the Collectron Browser

---
+++ packages/app-collection-browser/CollectionBrowser.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+// import Collection from '@emc/collection';
+
+const ItemList = observer(class ItemList extends Component {
+
+    static get defaultProps() {
+        return {
+          collection: {
+            items: [],
+          }
+        }
+    };
+
+    render() {
+        console.log(this.props.collection.items);
+        return <div>asd</div>;
+    }
+
+});
+
+class CollectionBrowser extends Component {
+
+    collection = { items: ['a', 'b'] };
+
+    render() {
+        return <ItemList collection={this.collection} />;
+    }
+
+}
+
+export default CollectionBrowser;
